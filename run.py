0c77b4548616e54242840d15281648b318b2c28f
alexnet123 <75438030+alexnet123@users.noreply.github.com>
2022-10-25 03:01:23 +0300

    test11

--- run.py
+++ run.py
@@ -1,10 +1,22 @@
 import re
 import time
 
-txt = "hello hello {={ param }=} hello"
+#file
+#txt = "hello hello {={ param }=} hello"
+#parm
 rep = r"{={(.*)}=}"
+#
 param = "+++HHHHH+++"
 
-print(re.sub(rep, param, txt))
+#print(re.sub(rep, param, txt))
 
-time.sleep(30)
+#template file
+
+def parser():
+    file = open("project/templates/terraform/main.tf")
+    txt = file.read()
+    print(re.sub(rep, param, txt))
+    file.close()
+
+#time.sleep(30)
+parser()
